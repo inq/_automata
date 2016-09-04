02a80c3a35a69dc2eb556dec60303fd208a5c6ed
inkyu <gofiri@gmail.com>
2016-09-04 15:33:06 +0800

    parse single integer

--- test.hs
+++ test.hs
@@ -1,20 +1,35 @@
 module Main where
 
+import Data.Char (digitToInt)
+
 data Parser a = Parser { p :: Char -> Parser a -> (Maybe a, Parser a) }
 
-data Automata = CSI
+data Automata = AInt Int
   deriving Show
 
 csi :: Parser Automata
 csi = Parser func
   where
-    func 'e' i = (Nothing, csi')
+    func 'e' _ = (Nothing, csi')
     func _ i = (Nothing, i)
 
 csi' :: Parser Automata
 csi' = Parser func
   where
-    func '[' i = (Just CSI, i)
+    func '[' i = (Nothing, num 0)
+    func _ i = (Nothing, i)
+
+num :: Int -> Parser Automata
+num acc = Parser func
+  where
+    func c i
+      | c >= '0' && c <= '9' = (Nothing, num (acc * 10 + digitToInt c))
+      | otherwise = p (finalize acc) c i
+
+finalize :: Int -> Parser Automata
+finalize acc = Parser func
+  where
+    func 'R' i = (Just $ AInt acc, i)
     func _ i = (Nothing, i)
 
 main :: IO ()
