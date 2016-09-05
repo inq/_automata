a4869f8f063eea648c0a153627282ab413970063
inkyu <gofiri@gmail.com>
2016-09-06 02:45:52 +0800

    quick and dirty

--- test.hs
+++ test.hs
@@ -2,13 +2,16 @@
 
 module Main where
 
+import Debug.Trace (trace)
+
 data Parser a = Parser
-  { p ::
+  { p :: forall r.
       Char -> -- Input character
       (Consume, Maybe a, Maybe (Parser a))
   }
 
-data Consume = Consumed | Skipped
+data Consume = Consumed | Skipped | Errored
+  deriving Show
 
 data PRes a = Finished a | Running
   deriving Show
@@ -24,25 +27,38 @@ instance Functor Parser where
 instance Applicative Parser where
   pure f = Parser $ \_ -> (Skipped, Just f, Nothing)
   (Parser a) <*> (Parser b) = Parser $ \c ->
-    let (as, ar, _) = a c
-        (bs, br, _) = b c
-        next = Parser $ \c' -> let (bs', br', _) = b c' in (bs', ar <*> br', Nothing)
+    let (as, ar, an) = a c
+        (bs, br, bn) = b c
+        jr = case ar of
+          Just x -> x
+          Nothing -> error "no"
+        next = Parser $ \c' ->
+          let (bs', br', bi') = b c'
+          in case bi' of
+            Just jbi' -> (bs', ar <*> br', Just (jr <$> jbi'))
+            Nothing -> (bs', ar <*> br', Nothing)
     in case as of
-        Skipped -> (bs, ar <*> br, Nothing)
-        Consumed -> (as, undefined, Just next)
+        Skipped -> case bs of
+          Errored -> (Errored, Nothing, Nothing)
+          _ -> case bn of
+            Just jbn -> trace " - With Next" (bs, ar <*> br, Just (jr <$> jbn))
+            Nothing ->  trace " - Skipped" (bs, ar <*> br, Nothing)
+        Consumed -> case an of
+          Just an' -> trace (" - Consumed: " ++ [c]) (as, Nothing, Just (an' <*> (Parser b)))
+          Nothing ->  trace (" - Consumed: " ++ [c]) (as, Nothing, Just next)
+        Errored -> (Errored, Nothing, Nothing)
 
 char :: Char -> Parser Char
 char c = Parser func
   where
     func c'
-      | c == c' = (Consumed, Just c, Nothing)
-      | otherwise = (Skipped, Nothing, Nothing)
+      | c == c' = trace (" - char: " ++ [c]) (Consumed, Just c, Nothing)
+      | otherwise = trace (" - char: need '" ++ [c] ++ "' but '" ++ [c'] ++ "'")  (Errored, Nothing, Nothing)
 
 initP :: Parser Automata
-initP = pure one <*> char 'x'
+initP = pure five <*> char 'a' <*> char 'b' <*> char 'c' <*> char 'd' <*> char 'e'
   where
-    one x = AString [x]
-
+    five a b c d e = AString [a, b, c, d, e]
 
 main :: IO ()
 main = do
@@ -50,8 +66,11 @@ main = do
   where
     loop parser = do
       x <- getChar
-      let (_, res, nextP) = (p parser) x
+      putStrLn "-----------"
+      putStrLn $ "Input: " ++ [x]
+      let (a, res, nextP) = (p parser) x
       print res
-      loop $ case nextP of
-          Just next -> next
-          Nothing -> initP
+      print (a, res)
+      case nextP of
+          Just next -> loop next
+          Nothing -> error "FInished"
